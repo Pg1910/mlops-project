5f163615ebf16f175a86eb4ffc44c90fc9f3f744
Pg1910 <gguptapiyush45@gmail.com>
2026-03-13 21:23:23 +0530

    dvc setup with v2

--- proj1/mycode.py
+++ proj1/mycode.py
@@ -5,9 +5,10 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
         'Age': [25, 30, 35],
         'City': ['New York', 'Los Angeles', 'Chicago']}
 new_row_loc = { 'Name': 'David', 'Age': 40, 'City': 'Houston'}
-
+new_row_loc2 = { 'Name': 'Piyush', 'Age': 42, 'City': 'kurnool'}
 df = pd.DataFrame(data)
 df.loc[len(df)] = new_row_loc
+df.loc[len(df)] = new_row_loc2
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
 file_path = os.path.join(data_dir, 'sample_data.csv')
